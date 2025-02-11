a6489b22bfcdbff6cb561c3257e4bbf510a8e153
braticpetar <braticpetar44@proton.me>
2025-02-11 11:03:04 +0100

    Commented lines

--- data-structures/stack/simple-text-editor.c
+++ data-structures/stack/simple-text-editor.c
@@ -2,25 +2,30 @@
 #include <string.h>
 #include <stdlib.h>
 
+// Defining maximum length
 #define MAX_LENGTH 999999
 
+// We keep track of our current text (string), each individual change (array of strings) and the total amount of changes in our history
 typedef struct {
     char *text;
     char **history;
     int history_count;
 } TextEditor;
 
+// Function that initializes our TextEditor structure
 void initEditor(TextEditor *editor) {
     editor->text = (char *)calloc(1, MAX_LENGTH);
     editor->history = (char **)malloc(MAX_LENGTH * sizeof(char *));
     editor->history_count = 0;
 }
 
+// Function that adds current text to the history
 void saveHistory(TextEditor *editor) {
     editor->history[editor->history_count] = strdup(editor->text);
     editor->history_count++;
 }
 
+// Function that saves history and then appends user's input to our current text
 void append(TextEditor *editor, const char *string) {
     saveHistory(editor);
     size_t new_len = strlen(editor->text) + strlen(string) + 1;
@@ -28,12 +33,14 @@ void append(TextEditor *editor, const char *string) {
     strcat(editor->text, string);
 }
 
+// Function that prints a character in a specified index
 void print(TextEditor *editor, int char_index) {
     if (char_index > 0 && char_index <= (int)strlen(editor->text)) {
         printf("%c\n", editor->text[char_index - 1]);
     }
 }
 
+// Function that deletes a characters at specified index
 void delete(TextEditor *editor, int char_index) {
     size_t len = strlen(editor->text);
     if (char_index > len) {
@@ -43,6 +50,7 @@ void delete(TextEditor *editor, int char_index) {
     editor->text[len - char_index] = '\0';
 }
 
+// Function that undoes the last thing we've done
 void undo(TextEditor *editor) {
     if (editor->history_count > 0) {
         editor->history_count--;
@@ -51,6 +59,7 @@ void undo(TextEditor *editor) {
     }
 }
 
+// Frees the memory
 void freeEditor(TextEditor *editor) {
     free(editor->text);
     for (int i = 0; i < editor->history_count; i++) {
@@ -60,6 +69,7 @@ void freeEditor(TextEditor *editor) {
     free(editor);
 }
 
+// Function for handling user inpu
 void handleInput() {
     int q, operation, char_index;
     char temp[MAX_LENGTH];
